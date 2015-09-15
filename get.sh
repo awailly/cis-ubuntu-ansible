a5971a4e4533ab17d76cb47cec631b5d2b361798
Paul Chaignon <paul.chaignon@gmail.com>
2015-09-15 13:34:08 +0200

    Deployment script can be launched several times without errors Ignores some steps that were already performed such as cloning the repository or creating the playbook.yml file Hashes the configuration file to make sure that it wasn't changed by the user

--- get.sh
+++ get.sh
@@ -16,15 +16,22 @@ sudo pip install ansible markupsafe
 
 mkdir -p ansible/roles-ubuntu/roles
 cd ansible/roles-ubuntu
-git clone https://github.com/awailly/cis-ubuntu-ansible.git roles/cis
+if [ ! -e "roles/cis" ]; then
+  git clone https://github.com/awailly/cis-ubuntu-ansible.git roles/cis
+fi
 
-cat >> playbook.yml << 'EOF'
+if [ ! -e "playbook.yml" ]; then
+  cat >> playbook.yml << 'EOF'
 ---
 - hosts: all
   roles:
     - cis
 EOF
+fi
 
-cp roles/cis/tests/desktop_defaults.yml roles/cis/vars/main.yml
+hash_vars=`md5sum roles/cis/vars/main.yml | awk '{ print $1 }'`
+if [ "$hash_vars" == "ebc1b1c57552ba806018f7231b33f30d" ]; then
+  cp roles/cis/tests/desktop_defaults.yml roles/cis/vars/main.yml
+fi
 
 ansible-playbook -b -u $USER $local_co -i "$IP," playbook.yml --ask-become-pass
